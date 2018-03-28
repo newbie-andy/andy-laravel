ccd581d0d220ebbb7ada8e7161ffa90ba13e0182
zhangshuo-test <2638401898@qq.com>
2018-03-28 13:10:25 +0800

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>andy</title>
-    <link rel="stylesheet" href="./material.min.css">
-    <script src="./material.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('./mdl/material.min.css') }}">
+    <script src="{{ asset('./mdl/material.min.js')  }}"></script>
 </head>
 <body>
-    
+    <div class="wrap">
+        <!-- Always shows a header, even in smaller screens. -->
+        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header class="mdl-layout__header">
+            <div class="mdl-layout__header-row">
+            <!-- Title -->
+            <span class="mdl-layout-title">Title</span>
+            <!-- Add spacer, to align navigation to the right -->
+            <div class="mdl-layout-spacer"></div>
+            <!-- Navigation. We hide it in small screens. -->
+            <nav class="mdl-navigation mdl-layout--large-screen-only">
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+            </nav>
+            </div>
+        </header>
+        <div class="mdl-layout__drawer">
+            <span class="mdl-layout-title">Title</span>
+            <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            </nav>
+        </div>
+        <main class="mdl-layout__content">
+            <div class="page-content"><!-- Your content goes here --></div>
+        </main>
+        </div>
+    </div>
 </body>
 </html>
