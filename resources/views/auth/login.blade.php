41b64f40d8dbca351c75013b7b4342a9e4ecc762
zhangshuo-test <2638401898@qq.com>
2018-03-30 13:48:10 +0800

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,14 +5,15 @@
     <div class="page-content">
         <div class="form">
             <div class="auth-box">
-                <form action="">
+                <form method="POST">
                     <p>登录</p>
+                    {{ csrf_field() }}
                     <div class="mdl-textfield mdl-js-textfield">
-                        <input class="mdl-textfield__input" type="email" id="email">
+                        <input name="email" class="mdl-textfield__input" type="email" id="email">
                         <label class="mdl-textfield__label" for="email">请输入邮箱</label>
                     </div>
                     <div class="mdl-textfield mdl-js-textfield">
-                        <input class="mdl-textfield__input" type="text" id="password">
+                        <input name="password" class="mdl-textfield__input" type="password" id="password">
                         <label class="mdl-textfield__label" for="password">请输入密码</label>
                     </div>
                     <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored full-width">登录</button>
