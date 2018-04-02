1d8d673713a451ea7e97eded144be3bd1a98d32a
zhangshuo-test <2638401898@qq.com>
2018-04-02 16:06:32 +0800

    square

--- resources/views/web/dynamics/index.blade.php
+++ resources/views/web/dynamics/index.blade.php
@@ -4,6 +4,8 @@
 <main class="mdl-layout__content aligin-items">
     <div class="page-content">
         
+       
+    
     </div>
 </main>
 @endSection
--- resources/views/web/square/index.blade.php
+++ resources/views/web/square/index.blade.php
@@ -3,7 +3,153 @@
 @section('content')
 <main class="mdl-layout__content aligin-items">
     <div class="page-content">
-        广场
+        <ul class="mdl-list mdl-shadow--2dp square-box">
+            <li class="mdl-list__item square-li">
+                <header>
+                    <div class="user-box mdl-shadow--2dp">
+                        <img src="{{ asset('./images/singledog.jpg') }}" alt="">
+                    </div>
+                </header>
+                <article>
+                    <p>
+                    TensorFlow 开发者峰会：支持 Swift，更好的支持 JavaScript
+                    </p>
+                    <div class="bottom">
+                        <p>
+                            <span class="category">IOS</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE855;</i>
+                            <span class="post">4小时前</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE8F4;</i>
+                            <span class="views">135次</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE87E;</i>
+                            <span class="like">135次</span>
+                        </p>
+                    </div>
+                </article>
+            </li>
+            <li class="mdl-list__item square-li">
+                <header>
+                    <div class="user-box mdl-shadow--2dp">
+                        <img src="{{ asset('./images/singledog.jpg') }}" alt="">
+                    </div>
+                </header>
+                <article>
+                    <p>
+                    网易金融前端实习生电话面试整理
+                    </p>
+                    <div class="bottom">
+                        <p>
+                            <span class="category">IOS</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE855;</i>
+                            <span class="post">4小时前</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE8F4;</i>
+                            <span class="views">135次</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE87E;</i>
+                            <span class="like">135次</span>
+                        </p>
+                    </div>
+                </article>
+            </li>
+            <li class="mdl-list__item square-li">
+                <header>
+                    <div class="user-box mdl-shadow--2dp">
+                        <img src="{{ asset('./images/singledog.jpg') }}" alt="">
+                    </div>
+                </header>
+                <article>
+                    <p>
+                    axios请求超时,设置重新请求的完美解决方法
+                    </p>
+                    <div class="bottom">
+                        <p>
+                            <span class="category">IOS</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE855;</i>
+                            <span class="post">4小时前</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE8F4;</i>
+                            <span class="views">135次</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE87E;</i>
+                            <span class="like">135次</span>
+                        </p>
+                    </div>
+                </article>
+            </li>
+            <li class="mdl-list__item square-li">
+                <header>
+                    <div class="user-box mdl-shadow--2dp">
+                        <img src="{{ asset('./images/singledog.jpg') }}" alt="">
+                    </div>
+                </header>
+                <article>
+                    <p>
+                    转向Kotlin——基础知识
+                    </p>
+                    <div class="bottom">
+                        <p>
+                            <span class="category">IOS</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE855;</i>
+                            <span class="post">4小时前</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE8F4;</i>
+                            <span class="views">135次</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE87E;</i>
+                            <span class="like">135次</span>
+                        </p>
+                    </div>
+                </article>
+            </li>
+            <li class="mdl-list__item square-li">
+                <header>
+                    <div class="user-box mdl-shadow--2dp">
+                        <img src="{{ asset('./images/singledog.jpg') }}" alt="">
+                    </div>
+                </header>
+                <article>
+                    <p>
+                    老司机 iOS 周报 #13 | 2018-04-02
+                    </p>
+                    <div class="bottom">
+                        <p>
+                            <span class="category">IOS</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE855;</i>
+                            <span class="post">4小时前</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE8F4;</i>
+                            <span class="views">135次</span>
+                        </p>
+                        <p>
+                            <i class="material-icons">&#xE87E;</i>
+                            <span class="like">135次</span>
+                        </p>
+                    </div>
+                </article>
+            </li>
+        </ul>
     </div>
 </main>
 @endSection
