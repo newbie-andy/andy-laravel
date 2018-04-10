4d5a212c5ac9489ecd57ec352bc06be787d85c7b
zhangshuo-test <2638401898@qq.com>
2018-04-10 09:18:42 +0800

    update

---
+++ app/Http/Controllers/Wechat/CommonController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Wechat;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CommonController extends Controller
+{
+    //token的验证操作
+    private function checkSignature(Request $request) {
+        $signature = $request->get("signature");
+        $timestamp = $request->get("timestamp");
+        $nonce = $request->get("nonce");
+        $echostr = $request->get("echostr");
+    
+        $tmpArr = array($timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $signature ){
+            return $echostr;
+        }else{
+            return false;
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::group(['namespace' => 'Wechat','prefix' => 'api'], function () {
+        Route::get('/checkSignature', 'CommonController@checkSignature');  
+    });
+});
+
